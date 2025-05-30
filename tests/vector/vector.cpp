6ea3c6f22488b2ff187d384122553499e6095055
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-30 23:48:34 +0100

    Create equality test

--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -3,17 +3,9 @@
 #include "../../src/vector/vector.hpp"
 #include "../../src/vector/vector_ops.hpp"
 
-TEST_CASE("Vector addition works (in-place)", "[vector_add, in_place]") {
-    Vector v1 = {1, 1, 1};
-    const Vector v2 = {2, 2, 2};
-    VectorOps::add(v1, v2);
-    REQUIRE(VectorOps::equals(v1, {3, 3, 3}));
-}
-
-TEST_CASE("Vector addition works (out-of-place)", "[vector_add, out_of_place]") {
+// Testing VectorOps::equals
+TEST_CASE("Vector equals works", "[vector_eq]") {
     const Vector v1 = {1, 1, 1};
-    const Vector v2 = {2, 2, 2};
-    Vector v3;
-    VectorOps::add(v1, v2, v3);
-    REQUIRE(VectorOps::equals(v3, {3, 3, 3}));
+    const Vector v2 = {1, 1, 1};
+    REQUIRE(VectorOps::equals(v1, v2));
 }
