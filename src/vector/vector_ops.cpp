7121bb2e96cf540c31566adf0909f7b101cd99ea
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-30 19:55:47 +0100

    Define addition implementations

--- src/vector/vector_ops.cpp
+++ src/vector/vector_ops.cpp
@@ -4,4 +4,16 @@ namespace VectorOps {
     float dotProduct(const Vector& a, const Vector& b) {
         return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
     }
+
+    void add(const Vector& a, const Vector& b, Vector& out) {
+        out.x = a.x + b.x;
+        out.y = a.y + b.y;
+        out.z = a.z + b.z;
+    }
+
+    void add(Vector& a, const Vector& b) {
+        a.x += b.x;
+        a.y += b.y;
+        a.z += b.z;
+    };
 }
