ead8c0c6e1cfe05c8121a86e52223bf793ae41ce
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-30 23:49:27 +0100

    Create vector closeness test

--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -9,3 +9,10 @@ TEST_CASE("Vector equals works", "[vector_eq]") {
     const Vector v2 = {1, 1, 1};
     REQUIRE(VectorOps::equals(v1, v2));
 }
+
+// Testing VectorOps::isClose
+TEST_CASE("Vector closeness works (in-place)", "[vector_sub, in_place]") {
+    Vector v1 = {1, 1, 1};
+    const Vector v2 = {1 + (VectorOps::EPSILON / 10), 1 + (VectorOps::EPSILON / 10), 1 + (VectorOps::EPSILON / 10)};
+    REQUIRE(VectorOps::isClose(v1, v2));
+}
