1b9d2007c363c4b030430415988ed31ccd6b6604
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-30 23:49:47 +0100

    Create addition tests

--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -16,3 +16,19 @@ TEST_CASE("Vector closeness works (in-place)", "[vector_sub, in_place]") {
     const Vector v2 = {1 + (VectorOps::EPSILON / 10), 1 + (VectorOps::EPSILON / 10), 1 + (VectorOps::EPSILON / 10)};
     REQUIRE(VectorOps::isClose(v1, v2));
 }
+
+// Testing VectorOps::add
+TEST_CASE("Vector addition works (in-place)", "[vector_add, in_place]") {
+    Vector v1 = {1, 1, 1};
+    const Vector v2 = {2, 2, 2};
+    VectorOps::add(v1, v2);
+    REQUIRE(VectorOps::equals(v1, {3, 3, 3}));
+}
+
+TEST_CASE("Vector addition works (out-of-place)", "[vector_add, out_of_place]") {
+    const Vector v1 = {1, 1, 1};
+    const Vector v2 = {2, 2, 2};
+    Vector v3;
+    VectorOps::add(v1, v2, v3);
+    REQUIRE(VectorOps::equals(v3, {3, 3, 3}));
+}
