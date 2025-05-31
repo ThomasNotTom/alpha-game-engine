a451596abd50b23f59a0b1a44681936250cc3116
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 01:10:23 +0100

    Add documentation to rotate x method

--- src/vector/vector_ops.cpp
+++ src/vector/vector_ops.cpp
@@ -146,6 +146,13 @@ namespace VectorOps {
         a.z = (tempX * b.y) - (tempY * b.x);
     };
 
+    /**
+     * @brief Calculates a vector rotated about the x-axis, and places the result into another vector.
+     *
+     * @param a The vector to be rotated.
+     * @param theta The angle (degrees) to rotate.
+     * @param out The output vector.
+     */
     void rotateX(const Vector& a, float theta, Vector& out) {
         float sin = sinf(theta * M_PI / 180);
         float cos = cosf(theta * M_PI / 180);
