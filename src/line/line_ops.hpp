76fec2f9333667b5d0f405966d7ad65857585a87
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 02:03:43 +0100

    Add documentation to get finish method

--- src/line/line_ops.hpp
+++ src/line/line_ops.hpp
@@ -3,5 +3,11 @@
 #include "./line.hpp"
 
 namespace LineOps {
+    /**
+     * @brief Get the finish vector of the line.
+     *
+     * @param line The line.
+     * @param out The output vector.
+     */
     void getFinish(const Line& line, Vector& out);
 };
