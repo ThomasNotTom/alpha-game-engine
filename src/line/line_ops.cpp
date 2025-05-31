e54a3bcc0e28b3bf24eb492f4ee95020650e4a8b
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 02:08:55 +0100

    Define equals implementation

--- src/line/line_ops.cpp
+++ src/line/line_ops.cpp
@@ -2,6 +2,10 @@
 #include "../vector/vector_ops.hpp"
 
 namespace LineOps {
+    bool equals(const Line& a, const Line& b) {
+        return VectorOps::equals(a.base, b.base) && VectorOps::equals(a.extension, b.extension);
+    }
+
     void getFinish(const Line& line, Vector& out) {
         VectorOps::add(line.base, line.extension, out);
     };
