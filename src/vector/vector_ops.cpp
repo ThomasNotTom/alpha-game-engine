b6b8a3d70198f67446469051528d519cbc5ebc34
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 01:11:38 +0100

    Add documentation to rotate y method

--- src/vector/vector_ops.cpp
+++ src/vector/vector_ops.cpp
@@ -179,6 +179,13 @@ namespace VectorOps {
         a.z = tempY * sin + tempZ * cos;
     };
 
+    /**
+     * @brief Calculates a vector rotated about the y-axis, and places the result into another vector.
+     *
+     * @param a The vector to be rotated.
+     * @param theta The angle (degrees) to rotate.
+     * @param out The output vector.
+     */
     void rotateY(const Vector& a, float theta, Vector& out) {
         float sin = sinf(theta * M_PI / 180);
         float cos = cosf(theta * M_PI / 180);
