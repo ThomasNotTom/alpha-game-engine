f0ed8df4a595f2e28a3d4c4a1f790b2157cefe48
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 02:16:50 +0100

    Create equals method test

--- tests/line/line.cpp
+++ tests/line/line.cpp
@@ -5,6 +5,13 @@
 
 #include "../../src/vector/vector_ops.hpp"
 
+// Testing LineOps::equals
+TEST_CASE("Line equals works", "[line_eq]") {
+    const Line l1 = {{1, 1, 1}, {2, 2, 2}};
+    const Line l2 = {{1, 1, 1}, {2, 2, 2}};
+    REQUIRE(LineOps::equals(l1, l2));
+}
+
 // Testing LineOps::getFinish
 TEST_CASE("Line get finish works", "[line_get_finish]") {
     const Line l1 = {{1, 1, 1}, {2, 2, 2}};
