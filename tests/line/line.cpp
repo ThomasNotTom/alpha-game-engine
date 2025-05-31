44121df70a272d2c94f6cc094c8a532f184b0c7d
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-05-31 02:19:33 +0100

    Create is close method test

--- tests/line/line.cpp
+++ tests/line/line.cpp
@@ -12,6 +12,16 @@ TEST_CASE("Line equals works", "[line_eq]") {
     REQUIRE(LineOps::equals(l1, l2));
 }
 
+// Testing LineOps::isClose
+TEST_CASE("Line closeness works", "[line_close]") {
+    const float EPSILON_TENTH = VectorOps::EPSILON / 10;
+    const Line l1 = {
+        {1 + EPSILON_TENTH, 1 - EPSILON_TENTH, 1 + EPSILON_TENTH},
+        {2 - EPSILON_TENTH, 2 + EPSILON_TENTH, 2 - EPSILON_TENTH}};
+    const Line l2 = {{1, 1, 1}, {2, 2, 2}};
+    REQUIRE(LineOps::isClose(l1, l2));
+}
+
 // Testing LineOps::getFinish
 TEST_CASE("Line get finish works", "[line_get_finish]") {
     const Line l1 = {{1, 1, 1}, {2, 2, 2}};
