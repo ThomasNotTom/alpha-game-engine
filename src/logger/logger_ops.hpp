9ee155e1759c0889fec13eaa4a68a7464c62f65e
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-01 03:56:00 +0100

    Create helper methods

--- src/logger/logger_ops.hpp
+++ src/logger/logger_ops.hpp
@@ -1,8 +1,15 @@
+#pragma once
+
 #include <string>
 
 #include "./logger.hpp"
 
 namespace Logger {
+    void _prettyPrint(const char* colour, std::string prefix, std::string text);
+    void _print(std::string text);
+    void _printToTerminal(std::string text);
+    void _printToFile(std::string text);
+
     void log(std::string message);
     void warning(std::string message);
     void error(std::string message);
