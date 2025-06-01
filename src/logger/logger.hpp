f0a6cd1082e2b2527858dbc81665fcfb1bef1f09
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-01 03:56:36 +0100

    Create mode and output file attributes

--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -1,4 +1,8 @@
+#pragma once
+
 #include <cstdint>
+#include <sstream>
+#include <fstream>
 
 namespace Logger {
     enum Mode : uint8_t {
@@ -6,11 +10,14 @@ namespace Logger {
         FILE
     };
 
-    namespace Colors {
+    namespace Colours {
         constexpr const char* RED = "\033[31m";
         constexpr const char* GREEN = "\033[32m";
         constexpr const char* YELLOW = "\033[33m";
         constexpr const char* RESET = "\033[0m";
         constexpr const char* BOLD = "\033[1m";
     };
+
+    inline Mode mode = TERMINAL;
+    inline std::ofstream outputFile;
 };
