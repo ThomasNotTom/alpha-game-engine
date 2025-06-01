a178f93b93041aec7583dbd548873347b941f31f
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-01 03:31:23 +0100

    Create colors namespace

--- src/logger/logger.hpp
+++ src/logger/logger.hpp
@@ -5,4 +5,12 @@ namespace Logger {
         TERMINAL,
         FILE
     };
+
+    namespace Colors {
+        constexpr const char* RED = "\033[31m";
+        constexpr const char* GREEN = "\033[32m";
+        constexpr const char* YELLOW = "\033[33m";
+        constexpr const char* RESET = "\033[0m";
+        constexpr const char* BOLD = "\033[1m";
+    };
 };
