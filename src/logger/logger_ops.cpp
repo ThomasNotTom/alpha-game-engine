62706a2022ea3b5ff36ea8a07f250aa06f0e2a2d
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-01 03:32:18 +0100

    Move mode definition

---
+++ src/logger/logger_ops.cpp
@@ -0,0 +1,6 @@
+#include "./logger_ops.hpp"
+
+namespace Logger {
+    static Mode mode = Mode::TERMINAL;
+
+};
--- src/logger/logger_ops.hpp
+++ src/logger/logger_ops.hpp
@@ -3,8 +3,6 @@
 #include "./logger.hpp"
 
 namespace Logger {
-    static Mode mode = Mode::TERMINAL;
-
     void log(std::string message);
     void warning(std::string message);
     void error(std::string message);
