7874b9e18c625a6ec20fee156d820bd3e397db46
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2025-06-01 11:30:33 +0100

    Create documentation for initialisation method

--- src/logger/logger_ops.hpp
+++ src/logger/logger_ops.hpp
@@ -10,6 +10,11 @@ namespace Logger {
     void _printToTerminal(std::string text);
     void _printToFile(std::string text);
 
+    /**
+     * @brief Initialisation method that creates a folder and file if mode is set to Mode::FILE mode.
+     *
+     * @param mode The mode the logger will be set to.
+     */
     void init(Mode mode);
 
     void log(std::string message);
